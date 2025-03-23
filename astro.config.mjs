28e1b377f89b8c71c7d409b6ae873be424906cf5
davinceleecode <vincent.lee.c.flores@gmail.com>
2025-03-23 20:18:21 +0800

    Fix: Set output directory to docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: "static",
-  base: "/vincent-landing-page/", // Match this with your repo name
-  build: {
-    outDir: "docs", // Change output directory to docs
-  }
+  outDir: 'docs', // Change output directory to /docs
+  site: 'https://davinceleecode.github.io/', // Replace with your GitHub Pages URL
 });
