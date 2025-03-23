2727e3b4ef32ebdce31aec57cf815cd6b5266194
davinceleecode <vincent.lee.c.flores@gmail.com>
2025-03-23 19:36:22 +0800

    Configured Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: "static",  // Important for GitHub Pages
+    base: "/vincent-landing-page/", // Match this with your repo name
+});
